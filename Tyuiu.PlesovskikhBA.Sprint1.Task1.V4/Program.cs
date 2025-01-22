46b4e2fef9571c7973f0b2c58350001602a361cc
Tyuiu.PlesovskikhBA@outlook.com <Tyuiu.PlesovskikhBA@outlook.com>
2025-01-22 23:03:06 +0500

    Task1

---
+++ Tyuiu.PlesovskikhBA.Sprint1.Task1.V4/Program.cs
@@ -0,0 +1,37 @@
+﻿using Tyuiu.PlesovskikhBA.Sprint1.Task1.V4.Lib;
+namespace Tyuiu.PlesovskikhBA.Sprint1.Task1.V4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Плесовских Б. А. | СМАРТб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки в C#                                               *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #4                                                              *");
+            Console.WriteLine("* Выполнил: Плесовских Борис Александрович | СМАРТб-24-1                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные  *");
+            Console.WriteLine("* вычисляет результат по формуле (x+y)/x*y и печатает его на экране.    *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            double x, y;
+            Console.WriteLine("Введите значение X: ");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите значение Y: ");
+            y = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.Calculate(x, y));
+            Console.ReadLine();
+
+        }
+    }
+}
