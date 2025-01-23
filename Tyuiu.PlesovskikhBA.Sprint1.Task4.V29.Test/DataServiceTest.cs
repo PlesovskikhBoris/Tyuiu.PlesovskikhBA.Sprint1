dae5f9671d2d4fcb7183bcad20b6ef84fd23efde
Tyuiu.PlesovskikhBA@outlook.com <Tyuiu.PlesovskikhBA@outlook.com>
2025-01-23 23:47:08 +0500

    Task4

---
+++ Tyuiu.PlesovskikhBA.Sprint1.Task4.V29.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.PlesovskikhBA.Sprint1.Task4.V29.Lib;
+
+namespace Tyuiu.PlesovskikhBA.Sprint1.Task4.V29.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 2;
+            double y = 0.1;
+            double wait = 32.489;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.PlesovskikhBA.Sprint1.Task4.V29/Program.cs
@@ -0,0 +1,38 @@
+﻿using Tyuiu.PlesovskikhBA.Sprint1.Task4.V29.Lib;
+namespace Tyuiu.PlesovskikhBA.Sprint1.Task4.V29
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Плесовских Б. А. | СМАРТб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Арифметические операторы в C#                                     *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #29                                                             *");
+            Console.WriteLine("* Выполнил: Плесовских Борис Александрович | СМАРТб-24-1                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* вычисляет результат по формуле и печатает его на экране.                *");
+            Console.WriteLine("* Ответ округлите до 3 знаков после запятой.                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            double x;
+            Console.WriteLine("Значение X :                                                              *");
+            x = Convert.ToInt32(Console.ReadLine());
+            double y;
+            Console.WriteLine("Значение Y :                                                              *");
+            y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Результат уравнения: " + ds.Calculate(x, y));
+            Console.ReadLine();
+
+        }
+    }
+}
