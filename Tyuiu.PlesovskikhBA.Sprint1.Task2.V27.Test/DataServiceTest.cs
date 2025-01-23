b32cc7749455aebc6477c392ede95fbbb2a8e802
Tyuiu.PlesovskikhBA@outlook.com <Tyuiu.PlesovskikhBA@outlook.com>
2025-01-23 19:08:38 +0500

    Task2

---
+++ Tyuiu.PlesovskikhBA.Sprint1.Task2.V27.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.PlesovskikhBA.Sprint1.Task2.V27.Lib;
+namespace Tyuiu.PlesovskikhBA.Sprint1.Task2.V27.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            int x = 10;
+            var res = ds.CalculateSquarePerimetr(x);
+            Assert.AreEqual(40, res);
+        }
+    }
+}
---
+++ Tyuiu.PlesovskikhBA.Sprint1.Task2.V27/Program.cs
@@ -0,0 +1,34 @@
+﻿using Tyuiu.PlesovskikhBA.Sprint1.Task2.V27.Lib;
+namespace Tyuiu.PlesovskikhBA.Sprint1.Task2.V27
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Плесовских Б. А. | СМАРТб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Арифметические операторы в C#                                     *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #27                                                             *");
+            Console.WriteLine("* Выполнил: Плесовских Борис Александрович | СМАРТб-24-1                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные         *");
+            Console.WriteLine("* данные, выполняет указанные расчёты и печатает результат на экране      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int x;
+            Console.WriteLine("Длина одной стороны квадрата :                                            *");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Периметр квадрата: " + ds.CalculateSquarePerimetr(x));
+            Console.ReadLine();
+
+        }
+    }
+}
