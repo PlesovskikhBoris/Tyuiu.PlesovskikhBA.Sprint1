64a524132765213cb6b83912ad4fbec8d42bb73e
Tyuiu.PlesovskikhBA@outlook.com <Tyuiu.PlesovskikhBA@outlook.com>
2025-02-09 19:53:24 +0500

    Task7

---
+++ Tyuiu.PlesovskikhBA.Sprint1.Task7.V8.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.PlesovskikhBA.Sprint1.Task7.V8.Lib;
+namespace Tyuiu.PlesovskikhBA.Sprint1.Task7.V8.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 2;
+            double y = 3;
+            double wait = -6.489;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.PlesovskikhBA.Sprint1.Task7.V8/Program.cs
@@ -0,0 +1,51 @@
+﻿//Написать программу, которая вычисляет математическое выражение по исходным значениям данных, вводимых пользователем. Ответ округлите до 3 знаков после запятой.
+//
+//                             y
+//Формула z = x lnx + -----------------
+//                                x
+//                        cosx - ---
+//                                3
+
+using Tyuiu.PlesovskikhBA.Sprint1.Task7.V8.Lib;
+namespace Tyuiu.PlesovskikhBA.Sprint1.Task7.V8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Плесовских Б. А. | СМАРТб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #8                                                              *");
+            Console.WriteLine("* Выполнил: Плесовских Борис Александрович | СМАРТб-24-1                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет математическое выражение по       *");
+            Console.WriteLine("* исходным значениям данных, вводимых пользователем.                      *");
+            Console.WriteLine("* Ответ округлите до 3 знаков после запятой.                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                   y                                     *");
+            Console.WriteLine("* ФОРМУЛА:    z = x * lnx + -----------------                             *");
+            Console.WriteLine("*                                      x                                  *");
+            Console.WriteLine("*                               cos - ---                                 *");
+            Console.WriteLine("*                                      3                                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            double x;
+            Console.WriteLine("Значение X :                                                              *");
+            x = Convert.ToInt32(Console.ReadLine());
+            double y;
+            Console.WriteLine("Значение Y :                                                              *");
+            y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Ответ: " + ds.Calculate(x, y));
+            Console.ReadLine();
+        }
+    }
+}
